1068ae6e68752ca782a7ac92d4f07e2cffdb2e21
DDHost <41550297+DDHost@users.noreply.github.com>
2022-10-13 21:07:08 +0300

    ver-1.0.0

---
+++ calendricalCalc.js
@@ -0,0 +1,65 @@
+///
+/// Hybrid
+///
+
+const Hebrew = require('./calendars/hebrew/heb.js');
+const gregorian = require('./calendars/gregorian/greg.js');
+
+/**
+ * Gregorian year to Hebrew year
+ * @param {number} year Gregorian year
+ * @return {number} Hebrew year
+ */
+const gregYearToHebYear = (year) => year - 1240 + 5000;
+
+/**
+ * Convert Hebrew date to Gregorian date.
+ * @param {number} year Hebrew year
+ * @param {number} month Hebrew month
+ * @param {number} day Hebrew day
+ * @returns {object} Gregorian date = {year, month, day}
+ */
+const hebDateToGreg = (year, month, day) => gregorian.fixedToGreg(Hebrew.hebToFixed(year, month, day));
+
+/**
+ * Convert Gregorian date to Hebrew date.
+ * @param {number} year Gregorian year
+ * @param {number} month Gregorian month
+ * @param {number} day Gregorian day
+ * @returns {object} Hebrew date = {year, month, day}
+ */
+const gregDateToHeb = (year, month, day) => Hebrew.fixedToheb(gregorian.gregToFixed(year, month, day));
+
+/**
+ * Get jewish holidays by given month & year
+ * @param {number} year Gregorian year
+ * @param {number} month Gregorian month
+ * @returns {Array<object>} holidays
+ */
+const jewHoliByGreg = (year, month) => {
+    // Length of the Gregorian month.
+    const max = new Date(year, month, 0).getDate();
+
+    const start = gregDateToHeb(year, month, 1); // The start date in Hebrew.
+    const end = gregDateToHeb(year, month, max); // The end date in Hebrew.
+
+    // The last the of the Hebrew month in Gregorian format.
+    gregLastDay = hebDateToGreg(start.year, start.month, Hebrew.lastDayOfMonth(start.year, start.month)).day;
+
+    let holidays = [];
+    holidays.push(Hebrew.getHolidays(start.year, start.month));
+
+    // If Hebrew month is ends before the Gregorian month,
+    // Then add the next month holidays (padding).
+    if (start.month !== end.month) {
+        let h = Hebrew.getHolidays(end.year, end.month).filter((holi) => Hebrew.gimatriaToNumber(holi.sdate) < max - gregLastDay);
+        holidays.push(h);
+    }
+
+    return holidays;
+};
+
+console.log(jewHoliByGreg(2022, 12));
+
+// EXPORT
+module.exports = { jewHoliByGreg, gregDateToHeb, hebDateToGreg, gregYearToHebYear };
